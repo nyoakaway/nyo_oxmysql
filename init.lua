4476ad4b825ee069d618cc48faabfa22f21014e0
DeadShot <jood5910@gmail.com>
2022-04-14 20:43:47 -0300

    Update for v2.2.5 Oxmysql

--- init.lua
+++ init.lua
@@ -1,50 +1,43 @@
 local Proxy = module("vrp","lib/Proxy")
 local vRP = Proxy.getInterface("vRP")
 
--- STATIC
-
-local function blob2string(blob)
-  for i,c in pairs(blob) do
-    blob[i] = string.char(c)
-  end
-
-  return table.concat(blob)
-end
-
 -- METHODS
 local queries = {}
-local API = exports["oxmysql"]
 
 local function on_init(cfg)
-  return API ~= nil
+	return API ~= nil
 end
 
 local function on_prepare(name, query)
-    queries[name] = query
+	queries[name] = query
 end
 
 local function on_query(name, params, mode)
-  local query = queries[name]
-  query = query:gsub("%,",", ")
-  local _params = {_ = true}
-  for k,v in pairs(params) do _params[k] = v end
-  local r = async()
-  if mode == "execute" then
-    API:query(query, _params, function(data)
-      r(data or 0)
-    end)
-  elseif mode == "scalar" then
-    API:scalar(query, _params, function(scalar)
-      r(scalar)
-    end)
-  else
-    API:query(query,_params, function(rows)    
-      r(rows, #rows)
-    end)
-  end
-  return r:wait()
+	local query = queries[name]
+	query = query:gsub("%,",", ")
+	local _params = {_ = true}
+	for k,v in pairs(params) do _params[k] = v end
+	local r = async()
+	if mode == "execute" then
+		MySQL.query(query, _params, function(data)
+			r(data or 0)
+		end)
+	elseif mode == "executeAsync" then
+		MySQL.Async.fetchAll(query, _params, function(data)
+			r(data or 0)
+		end)
+	elseif mode == "scalar" then
+		MySQL.scalar(query, _params, function(scalar)
+			r(scalar)
+		end)
+	else
+		MySQL.query(query,_params, function(rows)    
+			r(rows, #rows)
+		end)
+	end
+	return r:wait()
 end
 
 async(function()
-  vRP.registerDBDriver("oxmysql", on_init, on_prepare, on_query)
+	vRP.registerDBDriver("oxmysql", on_init, on_prepare, on_query)
 end)
